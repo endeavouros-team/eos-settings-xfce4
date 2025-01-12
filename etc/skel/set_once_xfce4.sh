694e8e6120695c34e063723db08db3f59fee4859
Johannes Kamprad <archlinux@kamprad.net>
2025-01-12 19:38:04 +0100

    Update and rename set_once_xfce4.bash to set_once_xfce4.sh

--- etc/skel/set_once_xfce4.sh
+++ etc/skel/set_once_xfce4.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 bash "~/generate-xfce4-desktop-xml" > "~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
 rm "~/.config/autostart/set_once_xfce4.desktop" "~/set_once_xfce4.bash" "~/generate-xfce4-desktop-xml"
